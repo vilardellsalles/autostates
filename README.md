5119cecba8784e2b93d45a3017ea15e544690f4b
Francesc Vilardell <vilardell@ieec.cat>
2018-03-11 11:56:34 +0100

    Very first commit showing how AutoSates can be created

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# autostates
-Solver for interacting states machines
+# AutoStates
+
+Imagine that you have several devices. Each device can be described by a finite
+state machine. Now, you want that all the devices execute the proper
+transition, at the proper time, to achieve a desired goal. This is what
+AutoStates aims to solve, making use of
+[Networkx](https://networkx.github.io/).
+
+In this very first example ([lights.py](./lights.py), three lights have to be
+switched on consecutively. The result will be different at each execution,
+since there are many ways to switch three lights on. However, the program will
+always execute the required actions to achieve the desired goal and plot all
+the possible actions for each possible combination of light states:
+
+![Diagram with all possible actions in a group of three lights](./lights.png "Diagram with all possible actions in a group of three lights")
+
+You can try to change the number of lights, but I would recommend not to make
+it larger than 10, or the plot will take forever to be plotted. 
---
+++ lights.py
@@ -0,0 +1,106 @@
+from itertools import combinations, compress
+
+import networkx as nx
+
+import matplotlib.pyplot as plt
+
+
+class Model(nx.DiGraph):
+    def __init__(self, actions, *args, **kwargs):
+        self.actions = {action.name: action for action in actions}
+
+        stack = set()
+        for action in actions:
+            stack |= set(action.requires().items())
+            stack |= set(action.provides().items())
+
+        num_devices = len(dict(stack))
+        states = [tuple(sorted(c)) for c in combinations(stack, num_devices)]
+        valid_states = [len(dict(st)) == num_devices for st in states]
+
+        nodes = list(compress(states, valid_states))
+
+        edges = []
+        for action in actions:
+            requires = action.requires()
+            provides = action.provides()
+            for node in nodes:
+                if set(requires.items()) <= set(node):
+                    destination = dict(node)
+                    destination.update(provides)
+                    new_node = tuple(sorted(destination.items()))
+                    if new_node in nodes:
+                        edges += [(node, new_node, {"name": action.name})]
+
+        super().__init__(self, *args, **kwargs)
+        self.add_nodes_from(nodes)
+
+        self.add_edges_from(edges)
+
+
+class Switch():
+
+    def __init__(self, device, goal):
+        goal = goal.upper()
+        if goal not in ["ON", "OFF"]:
+            raise ValueError("Invalid goal: {}".format(goal))
+
+        self.device = device
+        self.goal = goal
+
+    @property
+    def name(self):
+        return "{}->{}".format(self.device, self.goal)
+
+    def requires(self):
+        goal = "OFF" if self.goal == "ON" else "ON"
+        return {"LIGHT_{}".format(self.device): goal}
+
+    def provides(self):
+        return {"LIGHT_{}".format(self.device): self.goal}
+
+    def execute(self):
+        print("Switching light {} {}".format(self.device, self.goal.lower()))
+
+
+def node_to_label(node):
+    label = ""
+    for light in node:
+        label += "1" if light[1] == "ON" else "0"
+
+    return label
+
+
+if __name__ == "__main__":
+
+    NUM = 3 
+
+    actions = [Switch(num, "ON") for num in range(1, NUM+1)]
+    actions += [Switch(num, "OFF") for num in range(1, NUM+1)]
+
+    m = Model(actions)
+
+    print("\nExample on how to switch {} lights on:\n".format(NUM))
+
+    status = {"LIGHT_{}".format(num): "OFF" for num in range(1, NUM+1)}
+    goal = {"LIGHT_{}".format(num): "ON" for num in range(1, NUM+1)}
+    path = nx.shortest_path(m, tuple(sorted(status.items())),
+                            tuple(sorted(goal.items())))
+    for step, (origin, destination) in enumerate(zip(path[:-1], path[1:]), 1):
+        name = m[origin][destination]["name"]
+        print("Step {}:".format(step), end=" ")
+        m.actions[name].execute()
+
+    print("\nOnce all lights are on, plot model graph\n")
+
+    layout = nx.spring_layout(m)
+    labels = {node: node_to_label(node) for node in m.nodes}
+    nx.draw_networkx(m, pos=layout, labels=labels, arrows=False)
+    if NUM <= 3:
+        edge_labels = {edge[:2]: edge[-1] for edge in m.edges.data("name")}
+        nx.draw_networkx_edge_labels(m, pos=layout, edge_labels=edge_labels,
+                                     label_pos=0.75)
+
+    plt.show()
+
+    print("Bye")
